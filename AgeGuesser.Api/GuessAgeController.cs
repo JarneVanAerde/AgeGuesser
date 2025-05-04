a39f704ed29e53c47dda53a8603c605b30f30859
Jarne Van Aerde <jarnevanaerde@MacBook-Pro-van-Jarne.local>
2025-05-04 14:59:53 +0200

    Added the IMemoryCache example

--- AgeGuesser.Api/GuessAgeController.cs
+++ AgeGuesser.Api/GuessAgeController.cs
@@ -1,19 +1,39 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace AgeGuesser.Api;
 
 [Route("api/guess-age")]
 [ApiController]
-public class GuessAgeController(HttpClient httpClient) : ControllerBase
+public class GuessAgeController(HttpClient httpClient, IMemoryCache memoryCache) : ControllerBase
 {
+    private record AgeGuessResponse(int Age);
+    
     [HttpGet]
     public async Task<ActionResult<string>> GuessAge([FromQuery] string name)
     {
-        var result = await httpClient.GetAsync($"https://api.agify.io?name={name}");
-        var content = await result.Content.ReadFromJsonAsync<AgeGuessResponse>();
-        
-        return Ok($"Hello {name}, your age is guessed to be around {content!.Age}.");
+        var ageGuessResponse = await GetWithMemoryCache(name);
+        return Ok($"Hello {name}, your age is guessed to be around {ageGuessResponse.Age}.");
     }
 
-    private record AgeGuessResponse(int Age);
+    private async Task<AgeGuessResponse> GetWithMemoryCache(string name)
+    {
+        var cacheKey = $"guessedAge:{name}";
+        var ageGuessResponse = await memoryCache.GetOrCreateAsync<AgeGuessResponse>(cacheKey, async _ =>
+            await GetGuessedAge(name));
+
+        return ageGuessResponse!;
+    }
+
+    private async Task<AgeGuessResponse> GetGuessedAge(string name)
+    {
+        var result = await httpClient.GetAsync($"https://api.agify.io?name={name}");
+        var content =  await result.Content.ReadFromJsonAsync<AgeGuessResponse>();
+        if (content is null)
+        {
+            throw new InvalidOperationException("No guessed age returned");
+        }
+
+        return content;
+    }
 }
--- AgeGuesser.Api/Program.cs
+++ AgeGuesser.Api/Program.cs
@@ -2,6 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddHttpClient();
 builder.Services.AddControllers();
+builder.Services.AddMemoryCache();
 
 var app = builder.Build();
 
