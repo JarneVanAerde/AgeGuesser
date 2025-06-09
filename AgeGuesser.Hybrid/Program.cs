234e79cf33dd1cb8351fae100f91dd4d32dee5b9
Jarne Van Aerde <jarnevanaerde@MacBook-Pro-van-Jarne.local>
2025-06-09 13:12:19 +0200

    added the AgeGuesser.Hybrid project

---
+++ AgeGuesser.Hybrid/Program.cs
@@ -0,0 +1,16 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHttpClient();
+builder.Services.AddControllers();
+
+builder.Services.AddHybridCache();
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = "localhost:6379";
+});
+
+var app = builder.Build();
+
+app.MapControllers();
+
+app.Run();
